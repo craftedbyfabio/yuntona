830c5a577bde0ab8cc8fe2442d049d85b009173a
craftedbyfabio <GitHub@web3.mozmail.com>
2026-04-15 00:47:45 +0100

    Fix Plausible CSP — move init to external script, no inline JS

---
+++ js/plausible-init.js
@@ -0,0 +1,5 @@
+// Plausible Analytics init (first-party proxy)
+window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+plausible.init({
+  endpoint: "/dv/api/event"
+});
